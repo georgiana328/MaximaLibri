18a01289f3d71487aa350a450f00665b66fc4ea5
Denisa Neciu <denisa_96_02@yahoo.com>
2019-04-23 22:01:15 +0300

    Controller + Templates

--- maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/controller/BookController.java
+++ maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/controller/BookController.java
@@ -1,11 +1,20 @@
 package com.maximaLibri.maximaLibriV2.controller;
 
 import com.maximaLibri.maximaLibriV2.model.Book;
+import com.maximaLibri.maximaLibriV2.model.BookRating;
+import com.maximaLibri.maximaLibriV2.model.RoleName;
+import com.maximaLibri.maximaLibriV2.model.User;
 import com.maximaLibri.maximaLibriV2.service.BookService;
+import com.maximaLibri.maximaLibriV2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import static com.maximaLibri.maximaLibriV2.controller.AddRoleToModel.addRoleToModel;
 
 @Controller
 @RequestMapping("/book")
@@ -14,9 +23,13 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value={"/",""})
     public String bookList( Model model) {
-        model.addAttribute("bookList", bookService.getAllBooks());
+        addRoleToModel(model);
+        model.addAttribute("bookList", bookService.getTop10());
         return "bookList";
     }
 
@@ -27,12 +40,47 @@ public class BookController {
         } else {
             model.addAttribute("book", new Book());
         }
-        return "/admin/bookEdit";
+        addRoleToModel(model);
+        return "editBook";
     }
 
     @RequestMapping(value="/admin/bookEdit", method = RequestMethod.POST)
-    public String bookEdit(Model model, Book book) {
+    public String bookEdit(Model model, @ModelAttribute Book book) {
         bookService.saveBook(book);
         return "index";
+    }
+
+    @RequestMapping(value="/show/{isbn}", method = RequestMethod.GET)
+    public String bookShow(Model model, @PathVariable(required = true, name = "isbn") String isbn) {
+        model.addAttribute("book",bookService.getBookAndRatingById(isbn));
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_USER.toString()))) {
+            model.addAttribute("role","ROLE_USER");
+            User user = userService.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+            BookRating bookRating = bookService.getBookRatingByUserAndIsbn(isbn,user.getId());
+            if(bookRating==null) model.addAttribute("userRating",0);
+            else model.addAttribute("userRating",bookRating.getBookRating());
+        }
+        else if(authentication.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.toString()))) {
+            model.addAttribute("role","ROLE_ADMIN");
+            User user = userService.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+            BookRating bookRating = bookService.getBookRatingByUserAndIsbn(isbn,user.getId());
+            if(bookRating==null) model.addAttribute("userRating",0);
+            else model.addAttribute("userRating",bookRating.getBookRating());
+        }
+        else {
+            model.addAttribute("role","ROLE_ANONYMOUS");
+        }
+
+        return "bookShow";
+    }
+
+    @RequestMapping(value="/show/{isbn}", method = RequestMethod.POST)
+    public String bookShowRateBook(Model model, @PathVariable(required = true, name = "isbn") String isbn,
+                                   @ModelAttribute(name = "userRating") Integer userRating) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        bookService.saveBookRating(user.getId(),isbn,userRating);
+        return "bookShow";
     }
 }
--- maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/controller/MainController.java
+++ maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/controller/MainController.java
@@ -1,11 +1,19 @@
 package com.maximaLibri.maximaLibriV2.controller;
 
 
+import com.maximaLibri.maximaLibriV2.model.RoleName;
 import com.maximaLibri.maximaLibriV2.service.BookService;
+import com.maximaLibri.maximaLibriV2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.maximaLibri.maximaLibriV2.controller.AddRoleToModel.addRoleToModel;
 
 @Controller
 public class MainController {
@@ -13,27 +21,45 @@ public class MainController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/")
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = {"/",""})
     public String root() {
-        return "index";
+        return "redirect:/index";
     }
 
     @GetMapping("/login")
     public String login(Model model) {
+        addRoleToModel(model);
         return "login";
     }
+
+
 
     @GetMapping("/user")
     public String userIndex() {
         return "user/index";
     }
 
-    @GetMapping("/home")
-    public String home() {return "home";}
+//    @GetMapping("/home")
+//    public String home() {return "home";}
 
     @GetMapping("/index")
     public String showHomePage(Model model) {
         model.addAttribute("bookList", bookService.getTop10());
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if(authentication.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_USER.toString()))) {
+//            model.addAttribute("role","ROLE_USER");
+//            //System.out.println("USER LOGGED");
+//        }
+//        else if(authentication.getAuthorities().contains(new SimpleGrantedAuthority(RoleName.ROLE_ADMIN.toString()))) {
+//            model.addAttribute("role","ROLE_ADMIN");
+//        }
+//        else {
+//            model.addAttribute("role","ROLE_ANONYMOUS");
+//        }
+        addRoleToModel(model);
         return "index";
     }
 }
