c13f4402338c53df365bd25e9c3ebc217ee9d16a
alexandra.murgoci <alexandra.murgoci@ibm.com>
2019-04-24 00:55:34 +0300

    fixed all security permissions and problems in views + ran integration tests

--- maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/dto/IUserHistoryItem.java
+++
@@ -1,8 +0,0 @@
-package com.maximaLibri.maximaLibriV2.dto;
-
-public interface IUserHistoryItem {
-    String getIsbn();
-    String getBook_Title();
-    String getBook_Author();
-    String getBook_Rating();
-}
--- maximaLibriV2/maximaLibriV2/src/main/java/com/maximaLibri/maximaLibriV2/model/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.maximaLibri.maximaLibriV2.model;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
